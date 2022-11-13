f182c9444fa4f8471a8586316bc4bb2dc4e174cd
Thomas Ruffino <thomasruffino1@gmail.com>
2022-11-13 07:30:44 -0800

    logic for state message

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { ReactDOM } from "react";
+
+//study module 66 & 67 in course 
+const seasonConfig = {
+    summer : {
+        text: 'Lets hit the beach',
+        iconName:'sun'
+    },
+    winter: {
+        text: 'Bur it is cold',
+        iconName: 'snowflake'
+    }
+}; 
 
 //logic for the getSeason output below we use the function with the props as input 
 const getSeason = (lat, month) => {
@@ -7,14 +20,16 @@ const getSeason = (lat, month) => {
     } else {
         return lat > 0 ? 'winter' : 'summer';
     };
-};
 
+}; 
 //add props for the state prop 
+
 const SeasonDisplay = props => {
     console.log(props.lat); 
     const season = getSeason(props.lat, new Date().getMonth()); 
-const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
-
+/*seasonconfig allows us to remove these 
+const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach' */
+const {text,iconName} = seasonConfig[season] /*return object {text, iconeName}*/
     return <div>
        <h1>{text}</h1>
     </div>
